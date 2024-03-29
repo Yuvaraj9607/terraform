97ca3b81b69b6d3c2577025bf5663bd3b41ad4ca
Yuvaraj M <yuvi9607@gmail.com>
2024-03-29 10:26:04 +0530

    Security_group

---
+++ sg.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "ap-southeast-1"
+}
+resource "aws_security_group" "example" {
+  name        = "example-security-group"
+  description = "Example security group for EC2 instances"
+
+  // Define your security group rules here
+  ingress {
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+ingress {
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+ingress {
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+egress {
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+}
