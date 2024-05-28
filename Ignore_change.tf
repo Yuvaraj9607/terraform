e532dea4b7ec29d70ed1725e697928543ac87f0d
Yuvaraj M <yuvi9607@gmail.com>
2024-05-28 09:08:57 +0530

    Ignore_changes attribute

---
+++ Ignore_change.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.40.0"
+    }
+  }
+}
+provider "aws" {
+  region  = "ap-south-1"
+   access_key = "access_key"
+   secret_key = "secret_key"
+}
+
+resource "aws_instance" "prod" {
+  ami           = "ami-0f58b397bc5c1f2e8"
+  instance_type = "t2.micro"
+  availability_zone = "ap-south-1a"
+  #availability_zone = "ap-south-1b"
+  tags = {
+    Name = "prod1"
+  }
+  lifecycle {
+    ignore_changes = [ tags ]
+  }
+}
