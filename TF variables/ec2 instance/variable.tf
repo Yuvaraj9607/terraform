6bd3cf3470c6648ecb1f38eab03f8ab127b854dc
Yuvaraj M <yuvi9607@gmail.com>
2024-05-30 08:17:22 +0530

    variable.tf

--- TF variables/ec2 instance/variable.tf
+++ TF variables/ec2 instance/variable.tf
@@ -29,3 +29,8 @@ variable "availability_zone" {
   type = string
   default = "ap-south-1a"
 }
+variable "tags" {
+    description = "Name of the instance"
+    type = string
+    default = "UAT"
+} 
